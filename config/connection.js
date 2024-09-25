936785fe6088de1948b23624f0f4556381cfcdc8
Kalen <167487804+Ksjames22@users.noreply.github.com>
2024-09-24 23:01:43 -0400

    ssl

--- config/connection.js
+++ config/connection.js
@@ -4,17 +4,25 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.DATABASE_URL) {
-  // Use the remote database URL
   sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    logging: false // Optional: Disable logging for cleaner output
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Use true in production for stricter SSL checks
+      },
+    },
   });
 } else {
-  // Fallback to local configuration (for local development)
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'postgres',
-    port: 5432
+    port: 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   });
 }
 
