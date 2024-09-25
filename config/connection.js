bb2eec5cb8a25aec9fca5cd24ae1d654e7f108b1
Kalen <167487804+Ksjames22@users.noreply.github.com>
2024-09-24 21:51:53 -0400

    completed changes ready for render

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// Import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// Create connection to database, pass in postgres information for username and password
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'postgres',
+    port: 5432
+  });
+}
+
+module.exports = sequelize;
